a79e32a131ca27412c255a36caa0c72a126b19fb
Mike <mikehsm@outlook.com>
2025-05-22 08:31:41 +0200

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -13,39 +13,31 @@
 #include "get_next_line.h"
 #include "stdio.h"
 
+int	find
+
 char	*get_next_line(int fd)
 {
-	char			*output;
-	size_t			bytes_read;
-	char			buf[BUFFER_SIZE];
-	ssize_t			i;
+	char	*buf;
+	ssize_t i;
+	size_t bytes_read;
 
 	if (fd < 0)
 		return (NULL);
-	output = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	if (!output)
-		return (NULL);
 	bytes_read = 0;
+	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
 	while (1)
 	{
-		while (bytes_read < BUFFER_SIZE)
-		{
-			i = read(fd, &buf[bytes_read], 1);
-			{
-				if (i < 0)
-				{
-					if (output)
-						free(output);
-					return (NULL);
-				}
-				return (output);
-			}
-			bytes_read++;
-		}
+		i = read(fd, buf, BUFFER_SIZE);
+			if (i < 0)
+				return (NULL);
+		bytes_read += i;
+
 		return (output);
 	}
 }
-/*
+
 int	main(void)
 {
 	char	*line2 = get_next_line(1000);
@@ -55,4 +47,4 @@ int	main(void)
 	char	*line1 = get_next_line(fd);
 	printf("Empty file: %s\n", line1);
 	close(fd);
-}*/
+}
